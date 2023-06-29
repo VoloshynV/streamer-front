ed18c37797422687664f8f9b8b460f800f063f96
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-06-29 12:59:57 +0200

    Format

--- app/new-streamer/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateStreamerForm } from '@/components/forms'
-
-export default function NewStreamerPage() {
-  return (
-    <main className='grid w-full gap-2 p-24'>
-      <CreateStreamerForm />
-    </main>
-  )
-}
