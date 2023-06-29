6cc88459759209aa4a37316e6e10ac23e478c43c
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-06-29 13:49:42 +0200

    Change description limit

--- components/forms/create-streamer-form.tsx
+++ components/forms/create-streamer-form.tsx
@@ -14,7 +14,7 @@ const formSchema = z.object({
   nickname: z.string().min(2).max(50),
   image: z.string().min(2).max(100),
   platform: z.string().min(1).max(10),
-  description: z.string().min(2).max(150),
+  description: z.string().min(2).max(250),
 })
 
 const CreateStreamerForm = () => {
