106be23899fc0992f8f302e9100c69d7a44c33bb
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-06-29 12:57:23 +0200

    Delete theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,7 @@
 'use client'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
-import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
@@ -11,9 +9,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
-        {children}
-      </ThemeProvider>
+      {children}
       <Toaster />
     </QueryClientProvider>
   )
